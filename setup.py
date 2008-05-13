86d90ea14aeb08be5cfe163ce9aa61ee76a9d12a
yanghatespam <yanghatespam@c39569b0-052f-0410-bda9-d35d95505ca6>
2008-05-13 16:31:45 +0000

    added setup.py, readme

---
+++ setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+from commons import setup
+
+pkg_info_text = """
+Metadata-Version: 1.1
+Name: gbookmark2delicious
+Version: 2.0
+Author: Yang Zhang
+Author-email: yaaang NOSPAM at REMOVECAPS gmail
+Home-page: http://gbookmark2delicious.googlecode.com/
+Download-url: http://code.google.com/p/gbookmark2delicious/downloads/list
+Summary: Google Bookmarks to Delicious
+License: MIT License
+Description: Import your Google Bookmarks into del.icio.us.
+Keywords: Python,utility,utilities,library,libraries,async,asynchronous,
+          IO,networking,network,socket,sockets,I/O,threading,threads,
+          thread
+Platform: any
+Classifier: Development Status :: 5 - Stable
+Classifier: Environment :: Console
+Classifier: Intended Audience :: End Users/Desktop
+Classifier: License :: OSI Approved :: MIT License
+Classifier: Operating System :: OS Independent
+Classifier: Programming Language :: Python
+Classifier: Topic :: Internet :: WWW/HTTP
+"""
+
+setup.run_setup( pkg_info_text, scripts = ['source/gbookmark2delicious.py'] )
